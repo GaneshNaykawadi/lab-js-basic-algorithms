ec27ab39861bab95aa4cf2f7538101000f69cc71
ganeshnaykawadi <ganeshnaykawadi@gmail.com>
2024-11-14 20:38:11 +0530

    First iteration completed.

--- js/index.js
+++ js/index.js
@@ -1,4 +1,18 @@
+
+/** 
+ // Author: Ganesh Naykawadi.
+ // Date Created: 14th Nov, 2024.
+**/
+
+console.log("I'm ready!");
+
 // Iteration 1: Names and Input
+let hacker1 = "WebDevOpen";
+let hacker2 = "Steve";
+
+console.log("The driver's name is "+hacker1);
+console.log("The navigator's name is "+hacker2);
+
 //
 // Iteration 2: Conditionals
 
